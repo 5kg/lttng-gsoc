19b6e73f5afd52e835b498ef4f06ed53d08f7fcb
Zifei Tong <zifeitong@gmail.com>
2013-07-15 00:13:54 +0800

    Add: code for instrumentation at arbitrary address

---
+++ dyninst/experiment/mutator_offset.cpp
@@ -0,0 +1,41 @@
+#include <BPatch.h>
+#include <BPatch_object.h>
+#include <BPatch_point.h>
+#include <BPatch_function.h>
+#include <vector>
+#include <cstdlib>
+
+int main (int argc, const char* argv[]) {
+    BPatch bpatch;
+
+    // argv[2] is muttee's file name, will be muttee's argv[0]
+    BPatch_process *proc = bpatch.processCreate(argv[2], argv + 2);
+
+    // Options to tune performance
+    char *s;
+    if ((s = getenv("SET_TRAMP_RECURSIVE")) && (strcmp(s, "true") == 0))
+        bpatch.setTrampRecursive(true);
+    if ((s = getenv("SET_SAVE_FPR")) && (strcmp(s, "false") == 0))
+        bpatch.setSaveFPR(false);
+
+    BPatch_object *ipa = proc->loadLibrary(argv[1]);
+    BPatch_image *image = proc->getImage();
+
+    std::vector<BPatch_function *> tracepoints, probes;
+
+    image->findFunction("do_stuff", tracepoints);
+    BPatch_function *tracepoint = tracepoints[0];
+    Dyninst::Address start, end;
+    tracepoint->getAddressRange(start, end);
+
+    image->findFunction("tpbench_no_arg", probes);
+    BPatch_function *probe = probes[0];
+
+    std::vector<BPatch_snippet*> args;
+    BPatch_funcCallExpr call_probe(*probe, args);
+    proc->insertSnippet(call_probe, (tracepoint->findPoint(start+6))[0]);
+
+    proc->detach(true);
+
+    return 0;
+}
